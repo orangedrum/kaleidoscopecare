9125d1d2b04d6b2fa533d3dad803499d4c36562c
Jean <orangedrum@users.noreply.github.com>
2019-04-17 02:11:02 -0500

    jeanspopupattempt
    i took out webflows js... incase that causes you any trouble

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+	$("#show").click(function(){
+		$("#transition-text").hide();
+	});
+
+	$("#hide").click(function(){
+	  $("#transition-text").show();
+	});
+
+});
